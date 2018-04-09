9b1aef7a0b8197e1a0a1ca8b30bc9e60f771c605
Surkov Kirill <surkov.k@mywish.io>
2018-04-09 16:15:54 +0300

    Update lastWillNotify.js

--- test/lastWillNotify.js
+++ test/lastWillNotify.js
@@ -3,69 +3,54 @@ let chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 chai.should();
 
-// const utils = require('./utils')
-const LastWill = artifacts.require("./LastWillNotify.sol");
+const LastWillNotify = artifacts.require("./LastWillNotify.sol");
+const incTime = seconds => new Promise(next =>
+    web3.currentProvider.sendAsync({jsonrpc: "2.0", method: "evm_increaseTime", params: [seconds], id: 0}, () =>
+        web3.currentProvider.sendAsync({jsonrpc: "2.0", method: "evm_mine", id: 0}, next)
+    )
+);
+const getTime = () => new Promise(next => web3.eth.getBlock("latest", (err, block) => next(block.timestamp)));
+const getBalance = address => new Promise(next => web3.eth.getBalance(address, (err, balance) => next(balance)));
 
-const DAY = 24 * 3600;
-let NOW, YESTERDAY, DAY_BEFORE_YESTERDAY, TOMORROW, DAY_AFTER_TOMORROW;
-
-const initTime = (now) => {
-    NOW = now;
-    YESTERDAY = now - DAY;
-    DAY_BEFORE_YESTERDAY = YESTERDAY - DAY;
-    TOMORROW = now + DAY;
-    DAY_AFTER_TOMORROW = TOMORROW + DAY;
-};
-
-initTime(Math.ceil(new Date("2017-10-10T15:00:00Z").getTime() / 1000));
-
-contract('LastWillNotify', function(accounts) {
+contract('LastWillNotify', async accounts => {
     const OWNER = accounts[0];
     const TARGET = accounts[1];
+    const contract = await LastWillNotify.deployed();
 
-    const increaseTime = addSeconds => {
-        return new Promise((resolve, reject) => {
-            web3.currentProvider.sendAsync(
-                {jsonrpc: "2.0", method: "evm_increaseTime", params: [addSeconds], id: 0},
-                function (error, result) {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        initTime(NOW + addSeconds);
-                        resolve(result);
-                    }
-                }
-            );
-        });
-    };
-    const snapshot = () => {
-        return new Promise((resolve, reject) => {
-            web3.currentProvider.sendAsync(
-                {jsonrpc: "2.0", method: "evm_snapshot", params: [], id: 0},
-                function (error, result) {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(result);
-                    }
-                }
-            );
-        });
-    };
-    const restore = (id) => {
-        return new Promise((resolve, reject) => {
-            web3.currentProvider.sendAsync(
-                {jsonrpc: "2.0", method: "evm_restore", params: [id], id: 0},
-                function (error, result) {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(result);
-                    }
-                }
-            );
-        });
-    };
+    it("test fallback", async () => {
+        let time = await contract.lastActiveTs();
+        await incTime(3600);
+        await contract.sendTransaction({value: "50", from: OWNER});
+        assert.equal((await contract.lastActiveTs()).toString(), time.toString());
+        await incTime(3600);
+        await contract.sendTransaction({value: "50", from: TARGET});
+        time = await getTime();
+        assert.equal((await contract.lastActiveTs()).toString(), time.toString());
+    });
 
+    it("test check", async () => {
+        let balances = [await getBalance(accounts[3]), await getBalance(accounts[4])];
+        await incTime(60);
+        await contract.check();
+        assert.equal((await getBalance(accounts[3])).toString(), balances[0].toString());
+        assert.equal((await getBalance(accounts[4])).toString(), balances[1].toString());
+        await incTime(60);
+        await contract.check();
+        assert.equal((await getBalance(accounts[3])).toString(), balances[0].add(25).toString());
+        assert.equal((await getBalance(accounts[4])).toString(), balances[1].add(75).toString());
+    });
 
+    it("test kill", async () => {
+        let balance = await getBalance(TARGET);
+        let gas = (await contract.kill.estimateGas({from: TARGET})) * 10**11;
+        await contract.kill({from: TARGET});
+        assert.equal(balance.toString(), (await getBalance(TARGET)).add(gas).toString());
+        let canSend = true;
+        try {
+            await contract.sendTransaction({value: "50", from: OWNER});
+        } catch(e) {
+            canSend = false;
+        }
+        assert.equal(canSend, false);
+    });
 });
