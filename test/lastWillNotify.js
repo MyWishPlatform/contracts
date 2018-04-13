1661ab47af09664c40010fa78870863d7b32935d
Surkov Kirill <surkov.k@mywish.io>
2018-04-13 19:52:32 +0300

    Update lastWillNotify.js

--- test/lastWillNotify.js
+++ test/lastWillNotify.js
@@ -12,12 +12,14 @@ const incTime = seconds => new Promise(next =>
 const getTime = () => new Promise(next => web3.eth.getBlock("latest", (err, block) => next(block.timestamp)));
 const getBalance = address => new Promise(next => web3.eth.getBalance(address, (err, balance) => next(balance)));
 
-contract('LastWillNotify', async accounts => {
+contract("LastWillNotify", async accounts => {
     const OWNER = accounts[0];
     const TARGET = accounts[1];
-    const contract = await LastWillNotify.deployed();
+    const SOMEDUDE = accounts[2];
+    let contract;
 
     it("test fallback", async () => {
+        contract = await LastWillNotify.new(accounts[1], [accounts[3], accounts[4]], [25, 75], 120, false);
         let time = await contract.lastActiveTs();
         await incTime(3600);
         await contract.sendTransaction({value: "50", from: OWNER});
@@ -52,5 +54,47 @@ contract('LastWillNotify', async accounts => {
             canSend = false;
         }
         assert.equal(canSend, false);
+    });
+
+    it("test fallback with service", async () => {
+        contract = await LastWillNotify.new(accounts[1], [accounts[3], accounts[4]], [25, 75], 120, true);
+        let time = await contract.lastActiveTs();
+        await incTime(3600);
+        await contract.sendTransaction({value: "20", from: SOMEDUDE});
+        assert.equal((await contract.lastActiveTs()).toString(), time.toString());
+        await incTime(3600);
+        await contract.sendTransaction({value: "40", from: OWNER});
+        time = await getTime();
+        assert.equal((await contract.lastActiveTs()).toString(), time.toString());
+        await incTime(3600);
+        await contract.sendTransaction({value: "40", from: TARGET});
+        time = await getTime();
+        assert.equal((await contract.lastActiveTs()).toString(), time.toString());
+    });
+
+    it("test check with service", async () => {
+        let balances = [await getBalance(accounts[3]), await getBalance(accounts[4])];
+        await incTime(60);
+        let checkFail = false;
+        try {
+            await contract.check({from: SOMEDUDE});
+        } catch(e) {
+            checkFail = true;
+        }
+        assert.equal(checkFail, true);
+        checkFail = false;
+        try {
+            await contract.check({from: TARGET});
+        } catch(e) {
+            checkFail = true;
+        }
+        assert.equal(checkFail, true);
+        await contract.check({from: OWNER});
+        assert.equal((await getBalance(accounts[3])).toString(), balances[0].toString());
+        assert.equal((await getBalance(accounts[4])).toString(), balances[1].toString());
+        await incTime(120);
+        await contract.check({from: OWNER});
+        assert.equal((await getBalance(accounts[3])).toString(), balances[0].add(25).toString());
+        assert.equal((await getBalance(accounts[4])).toString(), balances[1].add(75).toString());
     });
 });
